d0bb74fa347101dbe972d6b670a521af81f34e1e
Caolan Fanning <caolan.fanning@gmail.com>
2021-08-10 20:10:54 +0100

    calender

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useState } from "react";
 
 import {
 	SearchIcon,
@@ -8,7 +9,28 @@ import {
 	UsersIcon,
 } from "@heroicons/react/solid";
 
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
+import { DateRangePicker } from "react-date-range";
+
 const Header = () => {
+	const [searchInput, setSearchInput] = useState("");
+	const [numberInput, setNumberInput] = useState(1);
+	const [startDate, setStartDate] = useState(new Date());
+	const [endDate, setEndDate] = useState(new Date());
+
+	const selectionRange = {
+		startDate: new Date(),
+		endDate: new Date(),
+		key: "selection",
+	};
+
+	const handleSelect = (ranges) => {
+		setStartDate(ranges.selection.startDate);
+		setEndDate(ranges.selection.endDate);
+	};
+
 	return (
 		<header className='sticky top-0 z-50 p-5 grid grid-cols-3 bg-white shadow-md md:p-10'>
 			{/* Left */}
@@ -27,7 +49,9 @@ const Header = () => {
 				<input
 					className='pl-5 bg-transparent outline-none flex-grow text-s text-gray-600 placeholder-gray-400'
 					type='text'
+					value={searchInput}
 					placeholder='Start your search'
+					onChange={(e) => setSearchInput(e.target.value)}
 				/>
 				<SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
 			</div>
@@ -42,6 +66,39 @@ const Header = () => {
 					<UsersIcon className='h-6' />
 				</div>
 			</div>
+			{searchInput && (
+				<div className='flex flex-col col-span-3 mx-auto mt-3'>
+					<DateRangePicker
+						ranges={[selectionRange]}
+						minDate={new Date()}
+						rangeColors={["#FD5B61"]}
+						onChange={handleSelect}
+					/>
+					<div className='flex justify-between items-center'>
+						<h2 className='text-2xl flex-grow font-semibold'>
+							Number of Guests
+						</h2>
+						<UsersIcon className='h-6' />
+						<input
+							className=' w-12 pl-2 outline-none text-[#FD5B61]'
+							type='number'
+							value={numberInput}
+							min={1}
+							onChange={(e) => setNumberInput(e.target.value)}
+						/>
+					</div>
+					<div className='flex justify-center items-center space-x-1'>
+						<button
+							className='mt-5 text-gray-400 border hover:bg-gray-300 hover:text-white transition duration-100 ease-in-out h-12 w-[150px] rounded-lg'
+							onClick={() => setSearchInput("")}>
+							Cancel
+						</button>
+						<button className='mt-5 text-[#FD5B61] border border-[#FD5B61] hover:bg-[#fd5b61] hover:text-white transition duration-100 ease-in-out h-12 w-[150px] rounded-lg'>
+							Search
+						</button>
+					</div>
+				</div>
+			)}
 		</header>
 	);
 };
