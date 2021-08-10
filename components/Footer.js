5c0e3624b34a711e5b580691f6137e84657cae59
Caolan Fanning <caolan.fanning@gmail.com>
2021-08-10 18:45:17 +0100

    footer complete

---
+++ components/Footer.js
@@ -0,0 +1,112 @@
+const Footer = () => {
+	return (
+		<footer className='hidden sm:block bg-gray-100 p-[40px]'>
+			<div className='max-w-7xl mx-auto sm:flex sm:flex-col lg:grid lg:grid-cols-4 '>
+				<div className='text-xs text-gray-800'>
+					<h5 className='font-bold px-[12px] mt-[12px]'>ABOUT</h5>
+					<div className='grid grid-cols-3 lg:flex lg:flex-col'>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							How Airbnb works
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Newsroom
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Investors
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Airbnb Plus
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Airbnb Luxe
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							HotelTonight
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Airbnb for Work
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Made possible by Hosts
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Careers
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Founders' Letter
+						</p>
+					</div>
+					<div className='mt-5 border border-b-1 lg:hidden'></div>
+				</div>
+				<div className='text-xs  text-gray-800'>
+					<h5 className='font-bold px-[12px] mt-[12px]'>COMMUNITY</h5>
+					<div className='grid grid-cols-3 lg:flex lg:flex-col'>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Diversity & Belonging
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Airbnb Associates
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Frontline Stays
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Guest Referrals
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Airbnb.org
+						</p>
+					</div>
+					<div className='mt-5 border border-b-1 lg:hidden'></div>
+				</div>
+				<div className=' text-xs  text-gray-800'>
+					<h5 className='font-bold px-[12px] mt-[12px]'>ABOUT</h5>
+					<div className='grid grid-cols-3 lg:flex lg:flex-col'>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Host your home
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Host an Online Experience
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Host an Experience
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Responsible hosting
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Resource Centre
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Community Centre
+						</p>
+					</div>
+					<div className='mt-5 border border-b-1 lg:hidden'></div>
+				</div>
+				<div className=' text-xs  text-gray-800'>
+					<h5 className='font-bold px-[12px] mt-[12px]'>SUPPORT</h5>
+					<div className='grid grid-cols-3 lg:flex lg:flex-col'>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Our COVID-19 Response
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Help Centre
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Cancellation options
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Cancellation options
+						</p>
+						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
+							Trust & Safety
+						</p>
+					</div>
+					<div className='mt-5 border border-b-1 lg:hidden'></div>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Header from "../components/Header";
 import SmallCard from "../components/SmallCard";
 import MediumCard from "../components/MediumCard";
 import LargeCard from "../components/LargeCard";
+import Footer from "../components/Footer";
 
 export default function Home({ exploreData, cardsData }) {
 	return (
@@ -54,6 +55,7 @@ export default function Home({ exploreData, cardsData }) {
 					buttonText='Get Inspired'
 				/>
 			</main>
+			<Footer />
 		</div>
 	);
 }
