425400e7dd544cc555c9f8a11b8a85be573cceab
Caolan Fanning <caolan.fanning@gmail.com>
2021-08-10 16:49:25 +0100

    large card

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import HeroImage from "../components/HeroImage";
 import Header from "../components/Header";
 import SmallCard from "../components/SmallCard";
 import MediumCard from "../components/MediumCard";
+import LargeCard from "../components/LargeCard";
 
 export default function Home({ exploreData, cardsData }) {
 	return (
@@ -46,6 +47,12 @@ export default function Home({ exploreData, cardsData }) {
 						))}
 					</div>
 				</section>
+				<LargeCard
+					img='https://links.papareact.com/4cj'
+					title='The Greatest Outdoors'
+					description='Wishlists curated by Airbnb'
+					buttonText='Get Inspired'
+				/>
 			</main>
 		</div>
 	);
