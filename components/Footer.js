b1db89e0c66811daf687c80f93e50c98dad915ad
Caolan Fanning <caolan.fanning@gmail.com>
2021-08-11 10:37:26 +0100

    resusable styling

--- components/Footer.js
+++ components/Footer.js
@@ -5,102 +5,50 @@ const Footer = () => {
 				<div className='text-xs text-gray-800'>
 					<h5 className='font-bold px-[12px] mt-[12px]'>ABOUT</h5>
 					<div className='grid grid-cols-3 lg:flex lg:flex-col'>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							How Airbnb works
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Newsroom
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Investors
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Airbnb Plus
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Airbnb Luxe
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							HotelTonight
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Airbnb for Work
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Made possible by Hosts
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Careers
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Founders' Letter
-						</p>
+						<p className='footerInfoLink'>How Airbnb works</p>
+						<p className='footerInfoLink'>Newsroom</p>
+						<p className='footerInfoLink'>Investors</p>
+						<p className='footerInfoLink'>Airbnb Plus</p>
+						<p className='footerInfoLink'>Airbnb Luxe</p>
+						<p className='footerInfoLink'>HotelTonight</p>
+						<p className='footerInfoLink'>Airbnb for Work</p>
+						<p className='footerInfoLink'>Made possible by Hosts</p>
+						<p className='footerInfoLink'>Careers</p>
+						<p className='footerInfoLink'>Founders' Letter</p>
 					</div>
 					<div className='mt-5 border border-b-1 lg:hidden'></div>
 				</div>
 				<div className='text-xs  text-gray-800'>
 					<h5 className='font-bold px-[12px] mt-[12px]'>COMMUNITY</h5>
 					<div className='grid grid-cols-3 lg:flex lg:flex-col'>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Diversity & Belonging
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Airbnb Associates
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Frontline Stays
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Guest Referrals
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Airbnb.org
-						</p>
+						<p className='footerInfoLink'>Diversity & Belonging</p>
+						<p className='footerInfoLink'>Airbnb Associates</p>
+						<p className='footerInfoLink'>Frontline Stays</p>
+						<p className='footerInfoLink'>Guest Referrals</p>
+						<p className='footerInfoLink'>Airbnb.org</p>
 					</div>
 					<div className='mt-5 border border-b-1 lg:hidden'></div>
 				</div>
 				<div className=' text-xs  text-gray-800'>
 					<h5 className='font-bold px-[12px] mt-[12px]'>ABOUT</h5>
 					<div className='grid grid-cols-3 lg:flex lg:flex-col'>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Host your home
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Host an Online Experience
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Host an Experience
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Responsible hosting
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Resource Centre
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Community Centre
-						</p>
+						<p className='footerInfoLink'>Host your home</p>
+						<p className='footerInfoLink'>Host an Online Experience</p>
+						<p className='footerInfoLink'>Host an Experience</p>
+						<p className='footerInfoLink'>Responsible hosting</p>
+						<p className='footerInfoLink'>Resource Centre</p>
+						<p className='footerInfoLink'>Community Centre</p>
 					</div>
 					<div className='mt-5 border border-b-1 lg:hidden'></div>
 				</div>
 				<div className=' text-xs  text-gray-800'>
 					<h5 className='font-bold px-[12px] mt-[12px]'>SUPPORT</h5>
 					<div className='grid grid-cols-3 lg:flex lg:flex-col'>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Our COVID-19 Response
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Help Centre
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Cancellation options
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Cancellation options
-						</p>
-						<p className='cursor-pointer hover:underline px-[12px] mt-[12px]'>
-							Trust & Safety
-						</p>
+						<p className='footerInfoLink'>Our COVID-19 Response</p>
+						<p className='footerInfoLink'>Help Centre</p>
+						<p className='footerInfoLink'>Cancellation options</p>
+						<p className='footerInfoLink'>Cancellation options</p>
+						<p className='footerInfoLink'>Trust & Safety</p>
 					</div>
 					<div className='mt-5 border border-b-1 lg:hidden'></div>
 				</div>
--- pages/search.js
+++ pages/search.js
@@ -6,6 +6,24 @@ const Search = () => {
 		<div>
 			<Header />
 
+			<main className='flex mt-10 '>
+				<section>
+					<p>300+ stays for 5 number of guests</p>
+
+					<h1 className='text-3xl font-semibold mt-2 mb-6'>
+						Stays in New York
+					</h1>
+
+					<div className='hidden lg:inline-flex space-x-2 mb-5'>
+						<p className='button'>Cancellation flexibilty</p>
+						<p className='button'>Type of place</p>
+						<p className='button'>Price</p>
+						<p className='button'>Room and Beds</p>
+						<p className='button'>More filters</p>
+					</div>
+				</section>
+			</main>
+
 			<Footer />
 		</div>
 	);
